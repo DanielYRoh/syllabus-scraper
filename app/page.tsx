ed578f6e59de242c1e1db574862a4f025a1e5983
DanielYRoh <danielroh05@gmail.com>
2025-04-05 00:34:45 -0700

    added gradient, changed colors

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,26 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
       <div className="bg-white min-h-screen">
-        <main style={{ background: '#fcb827' }} className="p-10">
-          <h1 className= "text-4xl font-bold text-center mt-20 text-blue-600">
+        <main style={{ background: 'linear-gradient(to bottom, #fcb827, #FFFFFF)' }} className="p-10">
+          <h1 style={{ color: '#047ac0' }} className= "text-6xl font-bold text-center mt-20">
                Welcome to Syllabus Scraper
           </h1>
 
-          <p className= "text-black text-1g mt-12 max-w-md mx-auto text-center"> Automatically scrape all your classes’ syllabus information with the push of a button! Send an email to your professor, view their ratemyprofessor, turn your classes into calendar events, and much more!
+          <p className= "text-black text-2xl mt-12 max-w-2xl mx-auto text-center"> Automatically scrape all your classes’ syllabus information with the push of a button! Send an email to your professor, view their ratemyprofessor, turn your classes into calendar events, and much more!
           </p>
           
           <button 
-          className="bg-yellow-300 text-blue-600 text-center font-bold px-4 py-2 mt-20 mx-auto block rounded hover:bg-blue-600 transition border-2 border-blue-600"
+           className="border-white bg-[#047ac0] text-white text-center font-bold px-4 py-2 mt-20 mx-auto block rounded hover:bg-blue-600 transition border-2"
           /*onClick={}*/>
             Start Scraping
-          </button>
+          </button> 
         </main>
 
         <div className="flex justify-center mt-10">
         <img
           src="https://content.sportslogos.net/logos/34/828/full/san_jose_state_spartans_logo_mascot_20002877.png"
           alt="sjsu"
-          className="w-auto h-auto"
+          className="w-64 h-140 object-cover mx-auto"
         />
         </div>
       </div>
