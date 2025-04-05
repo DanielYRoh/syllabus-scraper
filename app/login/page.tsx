5bec1a541f5c90bf88edab8e330d5c8d8cda65d1
epicgdog <91296067+epicgdog@users.noreply.github.com>
2025-04-04 23:38:26 -0700

    added suspense to the login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,11 @@
 import LoginForm from "@/app/components/login-form"
+import { Suspense } from "react"
 export default function Login() {
 
     return (
-        <LoginForm />
+        <Suspense>
+            <LoginForm />
+        </Suspense>
     )
 
 }
