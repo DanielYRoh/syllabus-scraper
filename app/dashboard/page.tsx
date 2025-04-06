63142d0884b8bc7ac2712f72722f244da0e62891
epicgdog <91296067+epicgdog@users.noreply.github.com>
2025-04-05 21:02:24 -0700

    added functionality to process pdfs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,26 @@
 import { signOut } from "@/auth";
-export default function Dashboard(){
+import { sendPDF } from "@/app/lib/actions"
+export default function Dashboard() {
     return (
         <div>
 
             <h1> Hi guys</h1>
             <form action={async () => {
                 "use server";
-                await signOut({ redirectTo: "/"})
+                await signOut({ redirectTo: "/" })
             }}>
                 <button> Sign me out</button>
+            </form>
 
+            <h1> Now you also gotta do me like that </h1>
+            <form action={sendPDF}>
+                <label htmlFor="pdf-importer">
+                    Import here with stuff
+                    <input id="pdf-importer" name="pdf-importer" type="file" className="bg-black text-white-800" />
+                </label>
+                <button className="bg-blue text-white:600">
+                    Send PDF
+                </button>
             </form>
         </div>
     )
